70862fa1cb95cc974219ad5bdd8f6dff20bf348b
Ifycode <marychord@gmail.com>
2025-10-13 21:03:35 +0100

    STRUCTURE: Base PWA setup & app structure setup

---
+++ public/assets/global.js
@@ -0,0 +1,7 @@
+
+// Register service worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service.worker.js').then(() => {
+        console.log('Service Worker Registered!!!');
+    });
+}
